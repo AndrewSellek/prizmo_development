bbadee8d1af46d6aa1d6e55d4e6b0a576731f708
AndrewSellek <sellek@strw.leidenuniv.nl>
2024-12-13 22:38:05 +0100

    Update default chemical network

--- src_py/prizmo_commons.py
+++ src_py/prizmo_commons.py
@@ -28,7 +28,7 @@ refInd_file = "../data/dust_refractive_index/silD03.txt"
 NCPAH = 100
 
 # chemical network file
-chemNet = "../networks/network_v2_withChex.dat"
+chemNet = "../networks/network_v2_final.dat"
 
 # atomic data file
 atomData = "../data/atomic_cooling/krome_data_singleH2.dat"
